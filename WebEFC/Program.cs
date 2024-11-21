9093785795bf6a1e79285899ce1c9ed72649a3f1
Sergey Shuykov <sshuykov@specialist.ru>
2024-11-21 16:15:23 +0300

    work with efc

---
+++ WebEFC/Program.cs
@@ -0,0 +1,47 @@
+using WebEFC.Models;
+
+namespace WebEFC
+{
+    public class Program
+    {
+        internal static IConfiguration config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddSqlServer<ApplicationContext>(
+                config.GetConnectionString("DefaultConnection"));
+            
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .WithStaticAssets();
+
+
+            app.Run();
+        }
+    }
+}
+
+
+
+
